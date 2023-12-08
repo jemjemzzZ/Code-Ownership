b54c2315f6c108250427bcf5f33a94fbf666936b
jemjemzzZ <81600131+jemjemzzZ@users.noreply.github.com>
2023-12-08 17:50:15 +1100

    Delete data_collection directory

--- data_collection/src/nvd_crawler/settings.py
+++
@@ -1,8 +0,0 @@
-NVD_API_KEY = "0afc16b9-0b01-4f98-994b-691a650918ea"
-NVD_APP_CONFIG = {
-	"distilled_dir":"../data/distilled/",
-	"raw_dir":"../data/raw/",
-    'frameworks': ['tensorflow','caffe','keras','opencv','pytorch', ''],
-    'nvd_search_api': "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=",
-    'headers': {"apiKey": NVD_API_KEY},
-}
--- data_collection/src/tf_security_advisory_parser/settings.py
+++
@@ -1,5 +0,0 @@
-APP_CONFIG = {
-    "distilled_dir":"../../data/distilled/",
-    "raw_dir":"../../data/raw/security_advisory_tensorflow/",
-    "output_file":"security_advisory_tensorflow.csv"
-}
