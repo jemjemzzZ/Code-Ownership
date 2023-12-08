e4c2afff69d4b26d7320671111cd178072fdabd9
jemjemzzZ <81600131+jemjemzzZ@users.noreply.github.com>
2023-12-08 17:50:25 +1100

    Delete result_analysis directory

--- result_analysis/code/settings.py
+++
@@ -1,6 +0,0 @@
-SETTINGS = {
-    'cve results': '../data/cve_results.csv',
-    'vulnerable results': '../data/pytorch_tensorflow_results.csv',
-    'non vulnerable results': '../data/non_vulnerable_results.csv',
-    'img folder': '../img/'
-}
